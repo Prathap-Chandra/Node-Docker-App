529be3068ebbd0b9fecd92037d7ac97f5f80601f
prathap_chandra_ <prathapchandra.moola@aerchain.io>
2022-11-20 20:40:19 +0530

    added some routes for testing

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 
-const PORT = 8642;
+const PORT = process.env.PORT || 8642;
 const HOST = 'http://localhost';
 
 app.get('/', (req, res) => {
     return res.status(200).json({
-        message: 'Hello World'
+        message: 'Hello World !!!'
+    });
+})
+
+app.get('/hi', (req, res) => {
+    return res.status(200).json({
+        message: 'Hi!!!'
+    });
+})
+
+app.get('/hey', (req, res) => {
+    return res.status(200).json({
+        message: 'Hey!!!'
+    });
+})
+
+app.get('/hello', (req, res) => {
+    return res.status(200).json({
+        message: 'Hello!!!'
     });
 })
 
